188f7102ab6312e94792f845ac8c12c57c58f0f4
Dominoed <Brandon.Postlethwait311@gmail.com>
2013-08-05 00:30:32 -0400

    Create ini.py

---
+++ ini.py
@@ -0,0 +1,27 @@
+import configparser
+
+def write(ifile,isec,ikey,ival):
+  'Write to ini file'
+	config = configparser.ConfigParser()
+	config[isec] = {ikey:ival}
+	with open(ifile,'w') as configfile:
+		config.write(configfile)
+def writemp(ifile,isec,ikey):
+	'Write to ini file'
+	config = configparser.ConfigParser()
+	config[isec] = {ikey}
+	with open(ifile,'w') as configfile:
+		config.write(configfile)
+def read(ifile,isec,ikey):
+	'Read from ini file'
+	config = configparser.ConfigParser()
+	config.read(ifile)
+	return config[isec][ikey]
+def secexists(ifile,isec):
+	config = configparser.ConfigParser()
+	config.read(ifile)
+	return isec in config
+def keyexists(ifile,isec,ikey):
+	config = configparser.ConfigParser()
+	config.read(ifile)
+	return ikey in isec in config
